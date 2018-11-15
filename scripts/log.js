d5f0cb3f88925fc690ab333e907063891f0f2a3b
yougotdave <yougotdave@gmail.com>
2018-11-15 15:19:36 +1100

    Major Revision
    Restructured code.
    Added Vue for data-binding and separation of concerns etc.

--- scripts/log.js
+++ scripts/log.js
@@ -13,100 +13,34 @@
 var log = (function(){
 
 	var logArray = [];
-	var length;
-	var selector;
 	
-	// ================================================================================
-	// Function: log.addEntry
-	// Parameters :
-	//	command : The instruction sent to the robot
-	// 	success : A boolean indicating success/failure of that command
-	// 	message : Used to pass on the error message
-	// --------------------------------------------------------------------------------
-	// Purpose:
-	// Add a new entry to the log.
-	// ================================================================================
-	var addEntry = function(command,success,message){
+	var addEntry = function(command,message){
 
-		// build into an object to contain success info
 		var entry = {
 			"command":command,
-			"success":success,
 			"message":message
 		};
 
 		logArray.push(entry);
 
 		// only keep most recent commands
-		if (logArray.length>=log.length){
+		if (logArray.length>=config.logLength){
 			logArray.shift();
 		}
-
-		// redraw the log
-		draw();
 	}; // End Function: log.addEntry
 
 
-	// ================================================================================
-	// Function: log.clear
-	// Parameters : none
-	// --------------------------------------------------------------------------------
-	// Purpose:
-	// Remove all entries from the log
-	// ================================================================================
 	const clear = function(){
 
 		// empty the log then redraw it
 		logArray = [];
-		draw();
-	}; // End Function: log.clear
-
-
-	// ================================================================================
-	// Function: log.draw
-	// Parameters : none
-	// --------------------------------------------------------------------------------
-	// Purpose:
-	// Draw the contents of the log on the GUI.
-	// --------------------------------------------------------------------------------
-	// Notes:
-	// This function is only used by the log object so has not been made public
-	// ================================================================================
-	const draw = function(){
-
-		// clear the log then recreate it
-		log.selector.empty();
-
-		for (var i = logArray.length - 1; i >= 0; i--) {
-			log.selector.append(`<li id='log_${i}' class='list-group-item'>${logArray[i].command} <span class="logMessage"></span></li>`);
-
-			// if the command failed then style it appropriately
-			if(!logArray[i].success){
-				jQuery(`#log_${i}`).addClass("commandFailed");
-				jQuery(`#log_${i}>.logMessage`).append(logArray[i].message);
-			}
-		}
-	}; // End Function: log.draw
-
-
-	// ================================================================================
-	// Function: log.init
-	// Parameters :
-	// 	length 	 : The number of entries to keep in the log.
-	// 	selector : The jQuery selector used to access the log element on the DOM
-	// --------------------------------------------------------------------------------
-	// Purpose:
-	// Initialise the log object with the supplied values.
-	// ================================================================================
-	var init = function(length,selector){
-		this.length = length;
-		this.selector = selector;
-	}; // End Function: log.init
+	};
 
 	// public functions/variables
 	return {
 		addEntry:addEntry,
 		clear:clear,
-		init:init
+		// init:init,
+		logArray:logArray
 	};
 })();
